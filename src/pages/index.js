ba4e4eaf439ae683f386a3ddcda5031b4dbce632
sanlaris.devm <sanlarisgame@gmail.com>
2023-01-19 13:10:03 +0100

    first link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -15,6 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <h1>
+          <a href="https://nextjs.org">JsDevter</a>
+        </h1>
+          <Link href="/timeline">
+            Timeline
+          </Link>
+
+
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
---
+++ src/pages/timeline/index.js
@@ -0,0 +1,7 @@
+export default function Timeline() {
+    return (
+      <>
+      Timelines
+      </>
+    )
+}
